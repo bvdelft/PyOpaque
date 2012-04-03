bbba576535a35a20d362e5136e45ac3fc4a911f3
Bart van Delft <vandeba@chalmers.se>
2012-04-03 17:30:37 +0200

    Added makefile, revised version numbering

--- src/setupcOpaque.py
+++ src/setupcOpaque.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 module1 = Extension('cOpaquemodule',
                     sources = ['cOpaquemodule.c'])          
 setup (name = 'cOpaque Module',
-       version = '1.0',
+       version = '0.0.1',
        description = 'Encapsulate python objects to make them opaque',
        ext_modules = [module1])
