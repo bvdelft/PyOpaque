54cfacd41b391bd467031667580332f223904745
Luciano Bello <luciano@debian.org>
2012-04-15 19:25:53 +0200

    working examples

--- examples/password.py
+++ examples/password.py
@@ -1,19 +1,22 @@
+import md5
+
 class password():
-     def __init__(self,username,password):
-          self.username = username
-          self.password = password
+     def __init__(self,password):
+          self.password =  md5.new(password).digest()
           self.triesLeft = 3
 
      def checkPassword(self,guessPassword):
      	if (self.triesLeft == 0):
      	  print "No more tries left!"
-     	  return
-		if (self.guessPassword == password) :
+          return
+        if self.password == md5.new(str(guessPassword)).digest():
          	self.triesLeft = 3
-         	print "very good!"
+         	print 'very good!'
         else:
+            print 'wrong!'
             self.triesLeft -= 1
 
-
-import opaque
-account=opaque.applyPolicy('opaque.cfg',account)
+'''Uncomment the following lines to protect the password field'''
+#import cOpaque
+#password=cOpaque.makeOpaque(password, [], ['password','triesLeft'] , True )
+pass1=password('thisisasecret')
