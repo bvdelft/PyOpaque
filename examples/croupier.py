903efc1c70c287b4ee0d615e05a8cb69adee8748
Luciano Bello <Luciano Bello bello@chalmers.se>
2012-06-25 19:23:39 +0200

    croupier example

--- examples/croupier.py
+++ examples/croupier.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
+from opaque import opaque
 from random import shuffle
 
+@opaque(private=['deck'])
 class Croupier(object):
 	def __init__(self):
 		self.deck=range(52)
 		shuffle(self.deck)
 	def drawCard(self):
 		return self.deck.pop()
-
 
